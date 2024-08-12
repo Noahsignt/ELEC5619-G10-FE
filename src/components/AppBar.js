16c96ce0fa25d3c1171d3373ce111584ec8dd969
Noah Vass <90555284+Noahsignt@users.noreply.github.com>
2024-08-13 09:08:55 +1000

    core: completed app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,18 +3,30 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
 
 export default function TopAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-            <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    News
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit'}} sx={{ flexGrow:1 }}>
+                <Typography variant="h6" component="div">
+                    EduBot
                 </Typography>
             </Link>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={() => {}}
+                color="inherit"
+              >
+                <AccountCircle />
+            </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
