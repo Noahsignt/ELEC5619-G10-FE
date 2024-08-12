ab51e9fabd68090cc0b43e7b137da676d7bcfe19
Noah Vass <90555284+Noahsignt@users.noreply.github.com>
2024-08-12 17:16:35 +1000

    core: homepage mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActions } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import './App.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Card sx={{ maxWidth: 600, margin: 'auto'}}>
+        <CardContent>
+          <Typography variant="h5" component="div" gutterBottom>
+            Unlock Your Potential
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Personalized, AI-driven learning experiences tailored to your unique journey. Whether you're just starting or diving into advanced concepts, our platform adapts to your knowledge level, helping you master new skills efficiently and affordably.
+          </Typography>
+          <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
+            Start learning today and take control of your education—no matter where you are or what you want to achieve.
+          </Typography>
+        </CardContent>
+        <Button variant="contained" color="primary" sx={{ m: 2 }}>
+          Register Now
+        </Button>
+      </Card>
+    </main>
+  )
 }
-
-export default App;
