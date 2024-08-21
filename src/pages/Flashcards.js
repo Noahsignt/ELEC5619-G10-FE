c8a1e86b763d9b1b310b93dbe514b2c2b7873bfd
noahsunpro <noah.vass@news.com.au>
2024-08-21 17:48:47 +1000

    core: finished flashcard UI

--- src/pages/Flashcards.js
+++ src/pages/Flashcards.js
@@ -1,31 +1,38 @@
 import TopAppBar from "../components/AppBar"
 import React, { useState } from 'react'
-import { Box, Typography, Card, CardContent, CardActionArea } from '@mui/material';
+import { Box, Typography, Card, CardContent, IconButton } from '@mui/material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 const testObj = [
     {front: 'Click me', back: 'This is the back now!'}
 ]
 
+
 const flipStyles = {
     container: {
-        perspective: '1000px', // Creates a 3D effect
+        perspective: '1000px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        height: '100vh', 
     },
     card: {
         width: '50vh',
         height: '40vh',
         position: 'relative',
         transformStyle: 'preserve-3d',
+        transform: 'rotateY(0deg)', 
+        transition: 'transform 1s ease',
         cursor: 'pointer',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        textAlign: 'center'
+        textAlign: 'center',
     },
     cardFlipped: {
-        transform: 'rotateY(180deg)',
+        transform: 'rotateY(180deg)', 
     },
     face: {
         position: 'absolute',
@@ -37,9 +44,13 @@ const flipStyles = {
         justifyContent: 'center',
         padding: '16px',
         boxSizing: 'border-box',
+        backgroundColor: 'primary.main', 
+        color: 'white',
     },
     back: {
         transform: 'rotateY(180deg)',
+        backgroundColor: 'white', 
+        color: 'primary.main', 
     },
 };
 
@@ -73,7 +84,19 @@ export default function Flashcards() {
         <main>
             <TopAppBar />
             <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
+                <IconButton>
+                    <ChevronLeftIcon fontSize="large" />
+                </IconButton>
                 <FlashCardDisplay front={testObj[0].front} back={testObj[0].back} />
+                <IconButton>
+                    <ChevronRightIcon fontSize="large" />
+                </IconButton>
+            </Box>
+            <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white',}}>
+                <PriorityHighIcon sx={{ backgroundColor: 'green', paddingTop: 2, paddingBottom: 2, paddingLeft: 2, borderTopLeftRadius: 4, borderBottomLeftRadius: 4}}/>
+                <Typography sx={{ backgroundColor: 'green', paddingTop: 2, paddingBottom: 2, paddingRight: 2, borderTopRightRadius: 4, borderBottomRightRadius: 4}}>
+                    Click on the card to flip it and click the arrows to navigate through your flashcards.
+                </Typography>
             </Box>
         </main>
     )
