67d78044d7251d67fd702eec86e311a5726d691c
noahsunpro <noah.vass@news.com.au>
2024-08-21 18:27:03 +1000

    core: topic validation checking FE

--- src/pages/NewTopic.js
+++ src/pages/NewTopic.js
@@ -1,9 +1,33 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import TopAppBar from '../components/AppBar'
 import { Box, TextField, Button, Typography } from '@mui/material'
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 
 export default function NewTopic() {
+    const [topic, setTopic] = useState("");
+    const [topicStatus, setTopicStatus] = useState(0);
+
+    const topicStatusMapping = [
+        {color: "none", text: ""},
+        {color: "warning.main", text: "Our AI needs more to work with. Please enter a longer text."},
+        {color: "warning.main", text: "Our AI system does not think this is a suitable topic to teach in the time provided. Please try something else."},
+        {color: "success.main", text: "Congrats! Our AI system has planned your course! We will redirect you now."}
+    ]
+
+    const onClick = () => {
+        if(topic.length <= 3) {
+            setTopicStatus(1);
+        }
+
+        if(topic === 'invalid topic') {
+            setTopicStatus(2);
+        }
+
+        if(topic === 'valid topic') {
+            setTopicStatus(3);
+        }
+    }
+
     return(
     <main>
         <TopAppBar />
@@ -12,6 +36,8 @@ export default function NewTopic() {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            justifyContent: 'center',
+            height: '80vh',
             flexGrow: 2,
             gap: 2
         }}
@@ -20,8 +46,15 @@ export default function NewTopic() {
             <Typography variant="h6" align="center" width="50vw">
                 Unlock the power of AI! Enter a topic you want to be taught on, and let our intelligent system guide you.
             </Typography>
-            <TextField id="outlined-basic" label="Enter topic here..." variant="outlined" />
-            <Button variant="contained">Generate</Button>
+            <TextField id="outlined-basic" onChange={(e) => setTopic(e.target.value)} label="Enter topic here..." variant="outlined" />
+            <Button onClick={onClick} variant="contained">Generate</Button>
+            {topicStatus !== 0 &&
+                <Box sx={{backgroundColor: topicStatusMapping[topicStatus].color, padding: 2, borderRadius: 1}}>
+                    <Typography color='white'>
+                        {topicStatusMapping[topicStatus].text}
+                    </Typography>
+                </Box>
+            }
         </Box>
     </main>)
 }
